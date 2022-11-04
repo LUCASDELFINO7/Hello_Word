e0252c788fa101fd2e5d19e259883e920075241d
LUCASDELFINO7 <lucasdelfino2511@gmail.com>
2022-11-04 11:40:06 -0300

    ATIVIDADE_NestJS_Hello_world

--- hello-word07-10/src/app.service.ts
+++ hello-word07-10/src/app.service.ts
@@ -5,10 +5,10 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  getBSM(): string {
-    return 'Responsabilidade Pessoal, Mentalidade de Crescimento, Orientação ao Futuro, Persistência ';
-}
-getObjetivo(): string {
-  return 'desenvolver ,organizar, praticar, usar. ';
-}
+  getBsm () : string {
+    return 'Bsm da Generation Brasil <br> <br> 1 - Responsabilidade Pessoal <br> 2 - Mentalidade de Crescimento <br> 3 - Orientação ao furuto <br> 4 - Persistência <br> 5 - Comunicação <br> 6 - Trabalho em equipe <br> 7 - Atenção para Detalhes <br> 8 - Proatividade <br> 9 - Profissionalismo <br> 10 - Precisão Técnica';
+  }
+  getObjetivo() : string {
+    return 'Objetivos de aprendizagem desta semana. <br> <br> 1 - Atenção para os detalhes <br> 2 - Trabalho em equipe <br> 3 - Profissionalismo';
+  }
 }
